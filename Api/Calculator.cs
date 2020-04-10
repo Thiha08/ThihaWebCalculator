2dd29b910818c4fea4d0f485af63f0c0a6b0529e
Thiha Kyaw Htin <thihakyaw.htin@haulio.io>
2020-04-10 13:12:12 +0630

    Add project files.

---
+++ Api/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Api
+{
+    public class Calculator
+    {
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        public int Subtract(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+
+        public int Divide(int value1, int value2)
+        {
+            if (value2 == 0)
+            {
+                throw new InvalidOperationException(
+                    "Argument cannot be zero.");
+            }
+
+            return value1 / value2;
+        }
+    }
+}
